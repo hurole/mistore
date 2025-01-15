b942b7276430caf3d338502c258a367ebcc20a93
hurole <1192163814@qq.com>
2025-01-15 22:51:17 +0800

    feat: message list

--- app/src/main/java/tech/cvfe/mistore/MainActivity.kt
+++ app/src/main/java/tech/cvfe/mistore/MainActivity.kt
@@ -2,56 +2,99 @@ package tech.cvfe.mistore
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import tech.cvfe.mistore.ui.theme.MistoreTheme
 
 
-val TAG = "MainActivity"
+const val TAG = "MainActivity"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MistoreTheme {
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.padding(WindowInsets.statusBars.asPaddingValues()).fillMaxSize()
+                Surface(
+                    color = MaterialTheme.colorScheme.surface,
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Greeting(
-                        name = "Android"
-                    )
+                    Column {
+                        MessageList(title = "消息列表")
+                    }
                 }
             }
-
         }
     }
 }
 
+data class Message(val title: String, val content: String)
+
 @Composable
-fun Greeting(name: String) {
+fun MessageList(title: String) {
     Text(
-        text = "Hello $name!",
+        text = title,
+        style = MaterialTheme.typography.titleLarge,
+        modifier = Modifier.padding(horizontal = 10.dp)
     )
-    Button(onClick = { Log.d(TAG, "Greeting: 123") }) {
-        Text("按钮")
+    MsgCard(
+        message = Message(
+            "Kotlin 开发",
+            "是的我是第几卡号 sd 卡恢复尽快哈卡号副卡收款方好服卡上卡号放假看啥"
+        )
+    )
+}
+
+@Composable
+fun MsgCard(message: Message) {
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        shadowElevation = 6.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp)
+    ) {
+        Row(modifier = Modifier.padding(16.dp, 10.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "标题",
+                modifier = Modifier
+                    .width(60.dp)
+                    .height(60.dp)
+                    .clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.padding(horizontal = 8.dp))
+            Column {
+                Text(text = message.title, style = MaterialTheme.typography.titleMedium)
+                Spacer(modifier = Modifier.padding(vertical = 1.dp))
+                Text(text = message.content, style = MaterialTheme.typography.bodySmall)
+            }
+        }
     }
 }
 
@@ -59,6 +102,13 @@ fun Greeting(name: String) {
 @Composable
 fun GreetingPreview() {
     MistoreTheme {
-        Greeting("Android")
+        Surface(
+            color = MaterialTheme.colorScheme.surface,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column {
+                MessageList(title = "消息列表")
+            }
+        }
     }
 }
